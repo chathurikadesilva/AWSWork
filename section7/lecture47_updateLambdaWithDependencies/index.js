62e4dc54dca4ca91d48577aef6e9116a7e8170f7
chathurika.desilva@gmail.com <chathurika.desilva@gmail.com>
2025-03-29 20:00:14 +1100

    commit work

---
+++ section7/lecture47_updateLambdaWithDependencies/index.js
@@ -0,0 +1,10 @@
+exports.handler = async (event) => {
+    console.log("EVENT: \n" + JSON.stringify(event, null,2))
+    const total = event.num1 + event.num2;
+    // TODO implement
+    const response = {
+      statusCode: 200,
+      body: "The total of " + event.num1 + " and " + event.num2 + " is " + total
+    };
+    return response;
+  };
